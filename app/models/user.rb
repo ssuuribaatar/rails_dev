4a62952fe102e2f2f5d0f81f3ee8df7115f49cb7
ssuuribaatar <ssuuribaatar@gmail.com>
2018-08-06 23:43:23 +0800

    edited

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-   validates :email, presence: true, format: { with: /A[^@s] @[^@s] z/, allow_blank: true } 
+    
+    validates :email, presence: true 
 	validates :password, presence: true, length: { minimum: 6 }	
  	validates :password_confirmation, presence: true, length: { minimum: 6 }
       
